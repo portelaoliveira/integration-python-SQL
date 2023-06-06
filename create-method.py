4b124ed216c3fa2c45cf558f0b55ef211f78b354
Danilo Portela <daniloportela97@gmail.com>
2023-06-06 16:53:27 -0300

    first commit

---
+++ create-method.py
@@ -0,0 +1,16 @@
+import pyodbc
+
+connection_data = "Driver={SQLite3 ODBC Driver};Server=localhost;Database=chinook.db"
+connection = pyodbc.connect(connection_data)
+print("Connection successful")
+
+cursor = connection.cursor()
+cursor.execute("""
+INSERT INTO albums (Title, ArtistId)
+VALUES
+('Lira Rock', 4)
+""")
+cursor.commit()
+cursor.close()
+connection.close()
+print("Connection finalized")
---
+++ integration-python-sql.py
@@ -0,0 +1,17 @@
+import pyodbc
+
+# print(pyodbc.drivers())
+
+connection_data = "Driver={SQLite3 ODBC Driver};Server=localhost;Database=salarios.sqlite"
+connection = pyodbc.connect(connection_data)
+print("Connection successful")
+
+cursor = connection.cursor()
+cursor.execute("SELECT * FROM Salaries")
+
+values = cursor.fetchall()
+print(values[:10])
+
+cursor.close()
+connection.close()
+print("Connection finalized")
