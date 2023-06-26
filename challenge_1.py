49efea0def476e0c703b71b746c06e058a1dd6b7
Danilo Portela <daniloportela97@gmail.com>
2023-06-26 17:03:14 -0300

    Desafio 1.

---
+++ challenge_1.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import sqlite3
+
+def format(value):
+    return f"R${value:.2f}"
+
+connection = sqlite3.connect("salarios.sqlite")
+salary_table = pd.read_sql("SELECT * FROM Salaries", connection)
+
+salary_table = salary_table.loc[salary_table["Agency"] == "San Francisco", :]
+mean_salary_table = salary_table.groupby("Year")[["TotalPay", "TotalPayBenefits"]].mean()
+table_count = salary_table.groupby("Year").count()
+table_count = table_count[["Id"]]
+table_count = table_count.rename(columns={"Id": "Quantidade"})
+table_total = salary_table.groupby("Year")[["TotalPay", "TotalPayBenefits"]].sum()
+table_total["TotalPay"] = table_total["TotalPay"].apply(format)
+table_total["TotalPayBenefits"] = table_total["TotalPayBenefits"].apply(format)
+
+# print(salary_table.info())
+# print(mean_salary_table)
+print(table_total)
+
+
+connection.close()
